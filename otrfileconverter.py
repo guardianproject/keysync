0ef88befa50215a1bb2a9df439194dbcf2506af9
Hans-Christoph Steiner <hans@eds.org>
2011-11-16 17:33:49 -0500

    rename to match the filename: otr.private_key

--- otrfileconverter.py
+++ otrfileconverter.py
@@ -6,7 +6,7 @@ from pyparsing import *
 from base64 import b64decode
 #import pprint
 
-from otr_private_keys import OTRPrivateKeys
+from otr_private_key import OTRPrivateKeys
 
 # TODO get Adium account IDs from ~/Library/Application\ Support/Adium\ 2.0/Users/Default/Accounts.plist
 # TODO use python-potr's convertkey.py to convert old libotr files
