f042fc0ecdc134134de8ae2e8cdfa27723d8b59b
Hans-Christoph Steiner <hans@eds.org>
2011-11-16 16:36:52 -0500

    split out otr.private_keys parsing

---
+++ otr_private_keys.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python2.6
+# -*- coding: utf-8 -*-
+
+from pyparsing import *
+from base64 import b64decode
+
+class OTRPrivateKeys():
+
+    @staticmethod
+    def verifyLen(t):
+        t = t[0]
+        if t.len is not None:
+            t1len = len(t[1])
+            if t1len != t.len:
+                raise ParseFatalException, \
+                        "invalid data of length %d, expected %s" % (t1len, t.len)
+        return t[1]
+
+    @staticmethod
+    def parse(filename):
+        # define punctuation literals
+        LPAR, RPAR, LBRK, RBRK, LBRC, RBRC, VBAR = map(Suppress, "()[]{}|")
+
+        decimal = Word("123456789",nums).setParseAction(lambda t: int(t[0]))
+        bytes = Word(printables)
+        raw = Group(decimal.setResultsName("len") + Suppress(":") + bytes).setParseAction(OTRPrivateKeys.verifyLen)
+        token = Word(alphanums + "-./_:*+=")
+        base64_ = Group(Optional(decimal,default=None).setResultsName("len") + VBAR 
+            + OneOrMore(Word( alphanums +"+/=" )).setParseAction(lambda t: b64decode("".join(t)))
+            + VBAR).setParseAction(OTRPrivateKeys.verifyLen)
+
+        hexadecimal = ("#" + OneOrMore(Word(hexnums)) + "#")\
+                        .setParseAction(lambda t: int("".join(t[1:-1]),16))
+        qString = Group(Optional(decimal,default=None).setResultsName("len") + 
+                                dblQuotedString.setParseAction(removeQuotes)).setParseAction(OTRPrivateKeys.verifyLen)
+        simpleString = raw | token | base64_ | hexadecimal | qString
+
+        display = LBRK + simpleString + RBRK
+        string_ = Optional(display) + simpleString
+
+        sexp = Forward()
+        sexpList = Group(LPAR + ZeroOrMore(sexp) + RPAR)
+        sexp << ( string_ | sexpList )
+
+        f = open(filename, 'r')
+        testotr = ""
+        for line in f.readlines():
+            testotr += line
+
+        try:
+            sexpr = sexp.parseString(testotr)
+            return sexpr.asList()
+        except ParseFatalException, pfe:
+            print "Error:", pfe.msg
+            print line(pfe.loc,t)
+            print pfe.markInputline()
--- otrfileconverter.py
+++ otrfileconverter.py
@@ -4,52 +4,10 @@
 import sys
 from pyparsing import *
 from base64 import b64decode
-import pprint
+#import pprint
 
-def verifyLen(t):
-    t = t[0]
-    if t.len is not None:
-        t1len = len(t[1])
-        if t1len != t.len:
-            raise ParseFatalException, \
-                    "invalid data of length %d, expected %s" % (t1len, t.len)
-    return t[1]
+from otr_private_keys import OTRPrivateKeys
 
-# define punctuation literals
-LPAR, RPAR, LBRK, RBRK, LBRC, RBRC, VBAR = map(Suppress, "()[]{}|")
+# TODO get Adium account IDs from ~/Library/Application\ Support/Adium\ 2.0/Users/Default/Accounts.plist
 
-decimal = Word("123456789",nums).setParseAction(lambda t: int(t[0]))
-bytes = Word(printables)
-raw = Group(decimal.setResultsName("len") + Suppress(":") + bytes).setParseAction(verifyLen)
-token = Word(alphanums + "-./_:*+=")
-base64_ = Group(Optional(decimal,default=None).setResultsName("len") + VBAR 
-    + OneOrMore(Word( alphanums +"+/=" )).setParseAction(lambda t: b64decode("".join(t)))
-    + VBAR).setParseAction(verifyLen)
-    
-hexadecimal = ("#" + OneOrMore(Word(hexnums)) + "#")\
-                .setParseAction(lambda t: int("".join(t[1:-1]),16))
-qString = Group(Optional(decimal,default=None).setResultsName("len") + 
-                        dblQuotedString.setParseAction(removeQuotes)).setParseAction(verifyLen)
-simpleString = raw | token | base64_ | hexadecimal | qString
-
-display = LBRK + simpleString + RBRK
-string_ = Optional(display) + simpleString
-
-sexp = Forward()
-sexpList = Group(LPAR + ZeroOrMore(sexp) + RPAR)
-sexp << ( string_ | sexpList )
-
-
-f = open(sys.argv[1], 'r')
-testotr = ""
-for line in f.readlines():
-    testotr += line
-
-try:
-    sexpr = sexp.parseString(testotr)
-    pprint.pprint(sexpr.asList())
-except ParseFatalException, pfe:
-    print "Error:", pfe.msg
-    print line(pfe.loc,t)
-    print pfe.markInputline()
-print
+print OTRPrivateKeys.parse(sys.argv[1])
