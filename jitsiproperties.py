70169959e92874aa2fcc7e1bc00bbeb19771b072
Hans-Christoph Steiner <hans@eds.org>
2011-11-17 16:42:30 -0500

    parsed public keys for the contacts

--- jitsiproperties.py
+++ jitsiproperties.py
@@ -4,6 +4,9 @@
 from pyjavaproperties import Properties
 import re
 
+# the accounts, private/public keys, and fingerprints are in sip-communicator.properties
+# the contacts list is in contactlist.xml
+
 class JitsiProperties():
 
     @staticmethod
@@ -17,23 +20,23 @@ class JitsiProperties():
         p.load(open(filename))
         ret = []
         for item in p.items():
-#            print "item: ",
-#            print item
-            if re.match('net\.java\.sip\.communicator\.impl\.protocol\.jabber\.acc[0-9]+\.ACCOUNT_UID',
-                        item[0]):
+            key = item[0]
+            if re.match('net\.java\.sip\.communicator\.impl\.protocol\.jabber\.acc[0-9]+\.ACCOUNT_UID', key):
                 id = JitsiProperties._parse_account_uid(item[1])
-                private_key_prop_key = ('net.java.sip.communicator.plugin.otr.'
-                                        + re.sub('[^a-zA-Z0-9_]', '_', item[1])
-                                        + '_privateKey')
+                prop_key = ('net.java.sip.communicator.plugin.otr.'
+                            + re.sub('[^a-zA-Z0-9_]', '_', item[1]))
+
+                private_key_prop_key = prop_key + '_privateKey'
                 private_key = p.getProperty(private_key_prop_key)
                 ret.append(('private-key', id, 'prpl-jabber', private_key))
-            
-            if item[0].startswith('net.java.sip.communicator.plugin.otr.'):
-                prop = item[0].split('.')[-1]
-                if prop.endswith('privateKey'):
-                    m = re.match('(.*)_privateKey', prop)
-                    ret.append(('private-key', m.group(1), item[1]))
-                print prop
+
+                public_key_prop_key = prop_key + '_publicKey'
+                public_key = p.getProperty(public_key_prop_key)
+                ret.append(('public-key', id, 'prpl-jabber', public_key))
+            elif (re.match('net\.java\.sip\.communicator\.plugin\.otr\..*_publicKey', key) and not
+                  re.match('net\.java\.sip\.communicator\.plugin\.otr\.(Jabber_|Google_Talk_)', key)):
+                prop = '.'.join(key.split('.')[-1].split('_')[0:-1])
+                ret.append(('fingerprint', prop, item[1]))
         return ret
 
 
