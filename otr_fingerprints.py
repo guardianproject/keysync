cc12f008198b4bb0e18b19e79b9dc966cb6fa163
Hans-Christoph Steiner <hans@eds.org>
2011-12-01 17:50:38 -0500

    created reusable OtrFingerprints class as a container for parse() function

--- otr_fingerprints.py
+++ otr_fingerprints.py
@@ -2,26 +2,31 @@
 # -*- coding: utf-8 -*-
 
 import csv
-import sys
-import pprint
 
+class OtrFingerprints():
+
+    @staticmethod
+    def parse(filename):
+        '''parse the otr.fingerprints file and return a list of keydicts'''
+        tsv = csv.reader(open(filename, 'r'), delimiter='\t')
+        keys = []
+        for row in tsv:
+            keydict = {}
+            if row[1].find('/') > -1:
+                name, resource = row[1].split('/')
+                keydict['name'] = name
+                keydict['resource'] = resource
+            else:
+                keydict['name'] = row[1]
+                keydict['resource'] = ''
+            keydict['protocol'] = row[2]
+            keydict['fingerprint'] = row[3]
+            keydict['verification'] = row[4]
+            keys.append(keydict)
+        return keys
 
 if __name__ == '__main__':
 
-    tsv = csv.reader(open(sys.argv[1], 'r'), delimiter='\t')
-    keys = []
-    for row in tsv:
-        keydict = {}
-        if row[1].find('/') > -1:
-            name, resource = row[1].split('/')
-            keydict['name'] = name
-            keydict['resource'] = resource
-        else:
-            keydict['name'] = row[1]
-            keydict['resource'] = ''
-        keydict['protocol'] = row[2]
-        keydict['fingerprint'] = row[3]
-        keydict['verification'] = row[4]
-        keys.append(keydict)
-    pprint.pprint(keys)
-
+    import sys
+    import pprint
+    pprint.pprint(OtrFingerprints.parse(sys.argv[1]))
