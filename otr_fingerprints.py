6f1c5e3b38f194b502fd2336ecfb01068fd0f405
Hans-Christoph Steiner <hans@eds.org>
2011-12-02 18:58:38 -0500

    BUGFIX: use the proper column for getting the remote user's ID from otr.fingerprints

--- otr_fingerprints.py
+++ otr_fingerprints.py
@@ -12,13 +12,7 @@ class OtrFingerprints():
         keys = []
         for row in tsv:
             keydict = {}
-            if row[1].find('/') > -1:
-                name, resource = row[1].split('/')
-                keydict['name'] = name.strip()
-                keydict['resource'] = resource.strip()
-            else:
-                keydict['name'] = row[1].strip()
-                keydict['resource'] = ''
+            keydict['name'] = row[0].strip()
             keydict['protocol'] = row[2].strip()
             keydict['fingerprint'] = row[3].strip()
             keydict['verification'] = row[4].strip()
