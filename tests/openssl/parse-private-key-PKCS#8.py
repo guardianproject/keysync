99c5fe2361fcc6b9220ceb975da3eb0c8e49e8b0
Hans-Christoph Steiner <hans@eds.org>
2012-10-15 18:41:51 -0400

    convert openssl PEM files into Java Properties and parse it in python

---
+++ tests/openssl/parse-private-key-PKCS#8.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import pyjavaproperties
+import imp
+
+errors = imp.load_source('errors', '../../otrapps/errors.py')
+util = imp.load_source('util', '../../otrapps/util.py')
+
+filename = 'private-key-PKCS#8.properties'
+p = pyjavaproperties.Properties()
+p.load(open(filename))
+for item in p.items():
+    if item[0] == 'privateKey':
+        privdict = util.ParsePkcs8(item[1])
+        print 'privdict: ',
+        print privdict
+    elif item[0] == 'publicKey':
+        pubdict = util.ParseX509(item[1])
+        print 'pubdict: ',
+        print pubdict
--- tests/openssl/useful-commands.sh
+++ tests/openssl/useful-commands.sh
@@ -11,3 +11,12 @@ openssl dsa -in private-key-openssl.pem -pubout -out public-key-PKCS\#1-X.509.pe
 
 # convert private key to a PKCS#8 format
 openssl pkcs8 -nocrypt -in private-key-openssl.pem -topk8 -out private-key-PKCS\#8.pem
+
+# convert PKCS#8 private key to a java properties file
+propfile=private-key-PKCS\#8.properties
+printf "privateKey=" > $propfile
+echo `cat private-key-PKCS#8.pem | grep -v -- '-----'` | sed 's, ,,g' | sed 's,=,\\=,g' >> $propfile
+
+# convert PKCS #1/X.509 public key to a java properties file
+printf "publicKey=" >> $propfile
+echo `cat public-key-pkcs1-X.509.pem | grep -v -- '-----'` | sed 's, ,,g' | sed 's,=,\\=,g' >> $propfile
