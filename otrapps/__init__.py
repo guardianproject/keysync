88c94efef233466fad8f0dc42a732afef9601d6b
Abel Luck <abel@guardianproject.info>
2013-03-16 12:11:27 +0100

    make the output dir if it does not exist

--- otrapps/__init__.py
+++ otrapps/__init__.py
@@ -1,2 +1,10 @@
 
 __all__ = ['adium', 'gibberbot', 'irssi', 'jitsi', 'pidgin', ]
+
+def make_outdir(output_folder, subdir):
+    dir = os.path.join(output_folder, subdir)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    return dir
+
+
