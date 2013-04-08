a24fc35f8db51f88bbecd5288130161906b32d51
Hans-Christoph Steiner <hans@eds.org>
2013-04-08 19:53:44 -0400

    pidgin: look up XMPP-Resource to include in otr.private_key 'name' field (closes #11)
    Pidgin uses the XMPP Resource as part of the account name for OTR private
    keys.  This patch takes the XMPP account name, and uses that to find the
    XMPP Resource that Pidgin associates with that account, so the account can
    be properly written out in otr.private_key.

--- otrapps/pidgin.py
+++ otrapps/pidgin.py
@@ -6,6 +6,7 @@ import platform
 import sys
 import plistlib
 import util
+from BeautifulSoup import BeautifulSoup
 
 from otr_private_key import OtrPrivateKeys
 from otr_fingerprints import OtrFingerprints
@@ -16,8 +17,30 @@ class PidginProperties():
         path = os.path.expanduser('~/Application Data/.purple')
     else:
         path = os.path.expanduser('~/.purple')
+    accountsfile = 'accounts.xml'
     keyfile = 'otr.private_key'
     fingerprintfile = 'otr.fingerprints'
+
+    @staticmethod
+    def _get_resources(settingsdir):
+        '''parse out the XMPP Resource from every Pidgin account'''
+        resources = dict()
+        accountsfile = os.path.join(settingsdir, PidginProperties.accountsfile)
+        if not os.path.exists(accountsfile):
+            print('Pidgin WARNING: No usable accounts.xml file found, add XMPP Resource to otr.private_key by hand!')
+            return resources
+        xml = ''
+        for line in open(accountsfile, 'r').readlines():
+            xml += line
+        for e in BeautifulSoup(xml)(text='prpl-jabber'):
+            pidginname = e.parent.parent.find('name').contents[0].split('/')
+            name = pidginname[0]
+            if len(pidginname) == 2:
+                resources[name] = pidginname[1]
+            else:
+                # Pidgin requires an XMPP Resource, even if its blank
+                resources[name] = ''
+        return resources
 
     @staticmethod
     def parse(settingsdir=None):
@@ -34,6 +57,13 @@ class PidginProperties():
         if os.path.exists(fpf):
             util.merge_keydicts(keydict, OtrFingerprints.parse(fpf))
 
+        resources = PidginProperties._get_resources(settingsdir)
+        for name, key in keydict.iteritems():
+            if key['protocol'] == 'prpl-jabber' \
+                    and 'x' in key.keys() \
+                    and name in resources.keys():
+                key['resource'] = resources[name]
+
         return keydict
 
     @staticmethod
@@ -43,11 +73,22 @@ class PidginProperties():
 
         kf = os.path.join(savedir, PidginProperties.keyfile)
         # Pidgin requires the XMPP resource in the account name field of the
-        # OTR private keys file, so we tack on a reasonable default.
+        # OTR private keys file, so fetch it from the existing account info
+        if os.path.exists(os.path.join(savedir, PidginProperties.accountsfile)):
+            accountsdir = savedir
+        elif os.path.exists(os.path.join(PidginProperties.path,
+                                         PidginProperties.accountsfile)):
+            accountsdir = PidginProperties.path
+        resources = PidginProperties._get_resources(accountsdir)
+
         pidginkeydict = dict()
         for name, key in keydict.iteritems():
-            if key['protocol'] == 'prpl-jabber':
-                key['name'] = key['name'] + '/Pidgin'
+            # pidgin requires the XMPP Resource in the account name for otr.private_keys
+            if key['protocol'] == 'prpl-jabber' and 'x' in key.keys():
+                if name in resources.keys():
+                    key['name'] = key['name'] + '/' + resources[name]
+                else:
+                    key['name'] = key['name'] + '/' + 'REPLACEME'
             pidginkeydict[name] = key
         OtrPrivateKeys.write(pidginkeydict, kf)
 
