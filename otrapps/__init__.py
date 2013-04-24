3d935b96e5e75ea6a636ffaaed1c80d9f39f94f8
Abel Luck <abel@guardianproject.info>
2013-04-24 16:28:58 +0200

    migrate from large if blocks towards unified otrapp handling
    This massively reduces the code cruft-factor and makes coding much easier and reduces
    chances of bugs creeping in from "changed X but not Y".
    
    Note the hidden --test flag to set the settings dir, use like:
    --test tests/

--- otrapps/__init__.py
+++ otrapps/__init__.py
@@ -1,8 +1,15 @@
 import os
 __all__ = ['adium', 'gibberbot', 'irssi', 'jitsi', 'pidgin', 'gajim',]
 
-apps_supported = __all__
-apps_supported.append('all')
+import adium, gibberbot, irssi, jitsi, pidgin, gajim
+apps = { 'adium'     : adium.AdiumProperties,
+         'gibberbot' : gibberbot.GibberbotProperties,
+         'irssi'     : irssi.IrssiProperties,
+         'jitsi'     : jitsi.JitsiProperties,
+         'pidgin'    : pidgin.PidginProperties,
+         'gajim'     : gajim.GajimProperties,
+        }
+apps_supported = apps.keys()
 
 def make_outdir(output_folder, subdir):
     dir = os.path.join(output_folder, subdir)
