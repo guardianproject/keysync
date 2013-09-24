4d1f13c1bd07aa9442b85d3d8e8ff7c5f4b82154
Hans-Christoph Steiner <hans@eds.org>
2013-09-24 19:58:56 -0400

    add test that merges all test files into each app's file, one at a time
    refs #1884
    refs #1888

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-cd ../otrapps
+tmpdir=`mktemp -d`
 
+echo '========================================================================'
+echo "Run each python file's __main__ tests"
+echo '========================================================================'
+cd ../otrapps
 for app in adium chatsecure gajim gpg irssi jitsi pidgin xchat util; do
     echo ''
     echo ''
@@ -13,3 +17,26 @@ for app in adium chatsecure gajim gpg irssi jitsi pidgin xchat util; do
     python ./$app.py
 done
 
+echo '========================================================================'
+echo "Merge all test files into an app's format"
+echo '========================================================================'
+cd ..
+for app in adium chatsecure gajim irssi jitsi pidgin xchat; do
+    echo '------------------------------------------------------------------------'
+    echo $app
+    echo '------------------------------------------------------------------------'
+    outdir=$tmpdir/merge-into-$app
+    mkdir $outdir
+    if [ $app = 'adium' ]; then
+        cp tests/adium/Accounts.plist $outdir/
+    elif [ $app = 'jitsi' ]; then
+        cp tests/jitsi/contactlist.xml $outdir/
+    fi
+    ./keysync --test tests/ \
+        -i adium -i irssi -i jitsi -i pidgin -i xchat \
+        -o $app \
+        --output-folder $outdir
+done
+
+echo $tmpdir
+ls -l $tmpdir
