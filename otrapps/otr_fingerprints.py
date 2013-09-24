7e188f8b7eba0d560fb587cca47144accc8ff03e
Hans-Christoph Steiner <hans@eds.org>
2013-09-24 16:48:56 -0400

    standardize on Python 3 style print() function to avoid confusing code
    This way we have the same print() syntax throughout the project, rather
    than one way in some files and another way in other files.

--- otrapps/otr_fingerprints.py
+++ otrapps/otr_fingerprints.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 import csv
 
 class OtrFingerprints():
--- otrapps/otr_private_key.py
+++ otrapps/otr_private_key.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 from pyparsing import *
 from base64 import b64decode
 import util
