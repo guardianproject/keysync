0f48ae686c87cb04d363184ca716e94a517f5c0f
Hans-Christoph Steiner <hans@eds.org>
2013-10-25 20:23:00 -0400

    set version to 0.2 for release

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ else:
 
 
 setup(name='keysync',
-    version='0.2.0beta3',
+    version='0.2',
     description='syncs OTR keys between different IM programs',
     long_description=read('README.md'),
     author='The Guardian Project',
